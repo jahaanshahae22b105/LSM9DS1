967eb1f11f60c0d516a39374a04c70db14dfbb19
jahaanshahae22b105 <ae22b105@smail.iitm.ac.in>
2023-04-28 17:53:08 +0530

    pushed

---
+++ imu.cpp
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS1.h>
+
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+
+void setup() {
+  
+  Serial.begin(9600);
+  SPI.begin();
+  
+  if (!lsm.begin()) {
+    Serial.println("Please Connect IMU");
+    while (1);
+  }
+}
+
+void loop() {
+  
+  event aevent, gevent, mevent;
+  lsm.readAccelerometer(aevent);
+  lsm.readGyro(gevent);
+  lsm.readMagnetometer(mevent);
+
+ 
+  Serial.println("Acceleration :");
+  Serial.print("X=");
+  Serial.print(accel_event.acceleration.x);
+  Serial.print(", Y=");
+  Serial.print(accel_event.acceleration.y);
+  Serial.print(", Z=");
+  Serial.println(accel_event.acceleration.z);
+  
+
+  Serial.println("Gyroscope : ");
+  Serial.print("X=");
+  Serial.print(gyro_event.gyro.x);
+  Serial.print(", Y=");
+  Serial.print(gyro_event.gyro.y);
+  Serial.print(", Z=");
+  Serial.println(gyro_event.gyro.z);
+  
+
+  Serial.println("Magnetometer : ");
+  Serial.print("X=");
+  Serial.print(mag_event.magnetic.x);
+  Serial.print(", Y=");
+  Serial.print(mag_event.magnetic.y);
+  Serial.print(", Z=");
+  Serial.println(mag_event.magnetic.z);
+
+  delay(100);
+}
